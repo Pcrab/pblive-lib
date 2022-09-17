8377d2510448a19abb5a6e88f66628f933810b20
Pcrab <pcrab@pcrab.xyz>
2022-09-17 21:43:25 +0800

    add optional handlers

--- src/index.ts
+++ src/index.ts
@@ -9,36 +9,35 @@ import {
     WatchedChangeRaw,
 } from "./types.js";
 
-const open = (roomId: number, handlers: MsgHandlers): void => {
+const open = (roomId: number, handlers?: MsgHandlers): void => {
     const live = new KeepLiveTCP(roomId);
     live.on("heartbeat", (count: number) => {
-        handlers.onHeartBeat?.(count);
+        handlers?.onHeartBeat?.(count);
     });
 
     live.on("DANMU_MSG", (danmu: DanmuRaw) => {
-        // console.log(JSON.stringify(danmu));
-        handlers.onDanmuMsg?.(parser.parseDanmu(danmu));
+        handlers?.onDanmuMsg?.(parser.parseDanmu(danmu));
     });
 
     live.on("INTERACT_WORD", (interactWord: InteractWordRaw) => {
-        handlers.onInteractWord?.(parser.parseInteractWord(interactWord));
+        handlers?.onInteractWord?.(parser.parseInteractWord(interactWord));
     });
 
     live.on("SEND_GIFT", (sendGift: SendGiftRaw) => {
-        handlers.onSendGift?.(parser.parseSendGift(sendGift));
+        handlers?.onSendGift?.(parser.parseSendGift(sendGift));
     });
 
     live.on("WATCHED_CHANGE", (watchedChange: WatchedChangeRaw) => {
-        handlers.onWatchedChange?.(parser.parseWatchedChange(watchedChange));
+        handlers?.onWatchedChange?.(parser.parseWatchedChange(watchedChange));
     });
 
     live.on("HOT_RANK_CHANGED_V2", (hotRankChanged: HotRankChangedRaw) => {
-        handlers.onHotRankChanged?.(parser.parseHotRankChanged(hotRankChanged));
+        handlers?.onHotRankChanged?.(
+            parser.parseHotRankChanged(hotRankChanged),
+        );
     });
 
     return;
 };
-
-open(10360, {});
 
 export default open;
